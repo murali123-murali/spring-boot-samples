52dddb46d292595826c2b7b65faa41d95cfcfcbb
murali123-murali <murlidarsi30@gmail.com>
2022-11-26 18:22:56 -0600

    PR request 1

--- spring-boot-mysql-jpa-hibernate/src/main/java/netgloo/controllers/MainController.java
+++ spring-boot-mysql-jpa-hibernate/src/main/java/netgloo/controllers/MainController.java
@@ -4,9 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-/**
- * This is pr
- */
+
 @Controller
 public class MainController {
 
